e39824e1a8aad15b14f88f24d109b2ee6c88826e
asmaesebti <msb.todos@gmail.com>
2022-05-07 16:41:22 +0200

    he agregado la nueva base de datos con los nuevos campos 07052022 16:41

--- proyectosis.sql
+++ proyectosis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 04-05-2022 a las 18:43:16
+-- Tiempo de generación: 07-05-2022 a las 14:39:38
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.3.21
 
@@ -30,8 +30,12 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `proyectosis`;
 CREATE TABLE IF NOT EXISTS `proyectosis` (
   `recu` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha_hoy` date DEFAULT NULL,
   `letype` varchar(50) NOT NULL,
+  `attestation` varchar(100) NOT NULL,
   `police` varchar(100) NOT NULL,
+  `matricule` varchar(50) NOT NULL,
+  `produit` varchar(20) NOT NULL,
   `assure` varchar(100) NOT NULL,
   `du` date NOT NULL,
   `au` date NOT NULL,
@@ -40,27 +44,22 @@ CREATE TABLE IF NOT EXISTS `proyectosis` (
   `cheque` decimal(10,2) NOT NULL,
   `autre` varchar(100) NOT NULL,
   `reste` decimal(10,2) NOT NULL,
+  `date_versement` date DEFAULT NULL,
+  `mode_paiment` varchar(20) NOT NULL,
   `cree_le` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`recu`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `proyectosis`
 --
 
-INSERT INTO `proyectosis` (`recu`, `letype`, `police`, `assure`, `du`, `au`, `totale`, `espece`, `cheque`, `autre`, `reste`, `cree_le`) VALUES
-(16, 'Changement vehicule', '11111www', 'wwwwwwwww', '2022-05-26', '2022-05-24', '1233.00', '123.00', '222.00', '', '888.00', '2022-05-04 17:12:25'),
-(2, 'Renouvellement', 'mohammed', 'sdrf', '2022-05-02', '2022-05-29', '120.00', '10.00', '110.00', 'sddd', '110.00', '2022-05-04 10:25:51'),
-(3, 'Autres', 'sebti', 'dd', '2022-05-25', '2022-05-19', '33.00', '0.00', '0.00', 'www', '0.00', '2022-05-04 10:28:52'),
-(18, 'Duplicata', '100.2020.000123', 'ahmed', '2022-05-21', '2022-05-20', '1234.00', '120.00', '130.00', '', '984.00', '2022-05-04 18:30:34'),
-(6, 'Renouvellement', 'ss', 'ss', '2022-05-25', '2022-05-25', '10.00', '0.00', '0.00', 'ss', '0.00', '2022-05-04 12:09:14'),
-(8, 'Affaire nouvelle', 'dd', 'ddd', '2022-05-20', '2022-05-22', '110.00', '0.00', '0.00', '', '0.00', '2022-05-04 12:25:14'),
-(9, 'Renouvellement', 'cfff', 'ffff', '2022-05-21', '2022-05-11', '10.00', '0.00', '0.00', '', '0.00', '2022-05-04 12:52:57'),
-(11, 'Changement vehicule', 'fffff', 'gggggg', '2022-05-13', '2022-05-18', '12.00', '0.00', '0.00', '', '0.00', '2022-05-04 13:11:24'),
-(12, 'Autres', 'ddd', 'dddd', '2022-05-27', '2022-05-24', '12.00', '0.00', '0.00', '', '0.00', '2022-05-04 13:12:46'),
-(13, 'Autres', 'dddddd', 'ddddd', '2022-05-27', '2022-06-02', '123.00', '0.00', '0.00', '', '0.00', '2022-05-04 13:29:36'),
-(14, 'Renouvellement', '100.2020.000121', 'nawfal sebti', '2022-05-18', '2022-05-29', '1200.00', '1000.00', '100.00', 'commentaire', '0.00', '2022-05-04 16:33:01'),
-(17, 'Renouvellement', 'qqqqq', 'qqqq', '2022-05-19', '2022-05-24', '12.00', '2.00', '3.00', '', '7.00', '2022-05-04 18:14:50');
+INSERT INTO `proyectosis` (`recu`, `fecha_hoy`, `letype`, `attestation`, `police`, `matricule`, `produit`, `assure`, `du`, `au`, `totale`, `espece`, `cheque`, `autre`, `reste`, `date_versement`, `mode_paiment`, `cree_le`) VALUES
+(24, '2022-05-04', 'Affaire nouvelle', 'dssaaa', '100.2020.000121', 'dssaaa', 'dssaaa', 'ahmed', '2022-05-24', '2022-05-18', '12.00', '2.00', '4.00', '', '6.00', '2022-05-28', 'dssaaa', '2022-05-06 18:38:31'),
+(25, '2022-05-09', 'Renouvellement', 'dssaaa', 'aassss', 'dssaaa', 'dssaaa', 'sssss', '2022-05-03', '2022-05-10', '14.00', '0.00', '0.00', '', '14.00', '2022-05-24', 'dssaaa', '2022-05-06 22:46:13'),
+(26, '2022-05-10', 'Renouvellement', 'dssaaa', 'aassss', 'dssaaa', 'dssaaa', 'sssss', '2022-05-03', '2022-05-10', '14.00', '0.00', '0.00', '', '14.00', '2022-05-17', 'dssaaa', '2022-05-06 22:46:16'),
+(27, '2022-05-18', 'Changement vehicule', 'dssaaa', 'ddfff', 'dssaaa', 'dssaaa', 'ffff', '2022-05-26', '2022-05-24', '22.00', '0.00', '0.00', '', '22.00', '2022-05-29', 'dssaaa', '2022-05-06 22:46:31'),
+(28, '2022-05-10', 'Changement vehicule', 'ggggg', 'hhhh', 'hhhh', 'hhhhh', 'hhhhh', '2022-05-14', '2022-05-29', '123.00', '0.00', '0.00', '', '123.00', '2022-05-17', 'jjjjj', '2022-05-07 11:52:59');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
